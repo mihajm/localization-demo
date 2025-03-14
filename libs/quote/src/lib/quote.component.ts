10b918e8838c71bf66c8f407f74a36037f0e0b3b
miham <miham@marand.si>
2025-03-14 10:26:53 +0100

    infra: stub demo libs

---
+++ libs/quote/src/lib/quote.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote',
+  imports: [CommonModule],
+  template: `<p>Quote works!</p>`,
+  styles: ``,
+})
+export class QuoteComponent {}
